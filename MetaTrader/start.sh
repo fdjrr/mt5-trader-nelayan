e43d78c7c8050d48b70bdd98df298912f2643fb5
Fadjrir Herlambang <fadjrir.co.id@gmail.com>
2025-10-24 18:24:38 +0700

    Remove mt5linux installation and server startup from the setup script

--- MetaTrader/start.sh
+++ MetaTrader/start.sh
@@ -88,39 +88,15 @@ fi
 # Upgrade pip and install required packages
 show_message "[6/7] Installing Python libraries"
 $wine_executable python -m pip install --upgrade --no-cache-dir pip
+
 # Install MetaTrader5 library in Windows if not installed
 show_message "[6/7] Installing MetaTrader5 library in Windows"
 if ! is_wine_python_package_installed "MetaTrader5"; then
     $wine_executable python -m pip install --no-cache-dir MetaTrader5
-fi
-# Install mt5linux library in Windows if not installed
-show_message "[6/7] Checking and installing mt5linux library in Windows if necessary"
-if ! is_wine_python_package_installed "mt5linux"; then
-    $wine_executable python -m pip install --no-cache-dir mt5linux
-fi
-
-# Install mt5linux library in Linux if not installed
-show_message "[6/7] Checking and installing mt5linux library in Linux if necessary"
-if ! is_python_package_installed "mt5linux"; then
-    pip install --upgrade --no-cache-dir mt5linux
 fi
 
 # Install pyxdg library in Linux if not installed
 show_message "[6/7] Checking and installing pyxdg library in Linux if necessary"
 if ! is_python_package_installed "pyxdg"; then
     pip install --upgrade --no-cache-dir pyxdg
-fi
-
-# Start the MT5 server on Linux
-show_message "[7/7] Starting the mt5linux server..."
-python3 -m mt5linux --host 0.0.0.0 -p $mt5server_port -w $wine_executable python.exe &
-
-# Give the server some time to start
-sleep 5
-
-# Check if the server is running
-if ss -tuln | grep ":$mt5server_port" > /dev/null; then
-    show_message "[7/7] The mt5linux server is running on port $mt5server_port."
-else
-    show_message "[7/7] Failed to start the mt5linux server on port $mt5server_port."
 fi
